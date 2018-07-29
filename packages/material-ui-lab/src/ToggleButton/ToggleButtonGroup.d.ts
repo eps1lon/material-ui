2dd9ea158c9f7489f0edf9fb62ec5e223e950d0f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-07-29 21:47:09 +0200

    [ToggleButtonGroup] make value type generic

--- packages/material-ui-lab/src/ToggleButton/ToggleButtonGroup.d.ts
+++ packages/material-ui-lab/src/ToggleButton/ToggleButtonGroup.d.ts
@@ -1,15 +1,44 @@
 import * as React from 'react';
 import { StandardProps } from '..';
 
-export interface ToggleButtonGroupProps
-  extends StandardProps<React.HTMLAttributes<HTMLDivElement>, ToggleButtonGroupClassKey> {
+interface CommonProps<T>
+  extends StandardProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      ToggleButtonGroupClassKey,
+      'onChange'
+    > {
   selected?: boolean;
-  exclusive?: boolean;
-  value?: any;
 }
+
+interface PropsExclusive<T> extends CommonProps<T> {
+  exclusive: true;
+  /**
+   * Triggered when a child triggers an onChange event.
+   * The value argument is null if the value did not change
+   */
+  onChange?: (value: T | null) => void;
+  /**
+   * the selected value
+   */
+  value?: T;
+}
+
+interface PropsNonExclusive<T extends Array<any>> extends CommonProps<T> {
+  exclusive?: false;
+  /**
+   * Triggered when a child triggers an onChange event.
+   * The value argument contains all values that are still selected or is null
+   * if no values are selected.
+   */
+  onChange?: (value: T | null) => void;
+  /**
+   * the selected values
+   */
+  value?: T;
+}
+
+export type ToggleButtonGroupProps<T> = PropsExclusive<T> | PropsNonExclusive<T[]>;
 
 export type ToggleButtonGroupClassKey = 'root' | 'selected';
 
-declare const ToggleButtonGroup: React.ComponentType<ToggleButtonGroupProps>;
-
-export default ToggleButtonGroup;
+export default class ToggleButtonGroup<T> extends React.Component<ToggleButtonGroupProps<T>> {}
