29ec1fa07550e5e92cf74065a046bc270df56438
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-12-19 18:14:04 +0100

    [scripts] Use sub command structure for mui-build

--- packages/scripts/bin/build.js
+++ packages/scripts/bin/build.js
@@ -3,28 +3,40 @@ const program = require('commander');
 const path = require('path');
 const { buildRollup, buildTargets, copyFiles, fullBuild } = require('../src/tasks');
 
+function rejectTask(getReason) {
+  // mocked Listr
+  return {
+    run: () => Promise.reject(new Error(getReason())),
+  };
+}
+
+let task = rejectTask(program.usage);
+
 program
   .version(require('../package.json').version)
-  .option('-c, --copy-files', 'copy files only')
-  .option('-f, --full', 'complete build pipeline')
-  .option('-t, --targets <target>', 'Target', arg => arg.split(','))
+  .command('package')
+  .option('-t, --targets <targets>', 'Targets', arg => arg.split(','))
   .option('-r, --rollup <configPath>', 'Executes rollup with the given config')
-  .parse(process.argv);
+  .action((cmd) => {
+    if (!cmd.targets || !cmd.targets.length) {
+      task = rejectTask(() => '`--targets` cannot be empty');
+    } else {
+      task = fullBuild(cmd.targets, { rollupConfig: cmd.rollup });
+    }
+  })
+  .command('copy-files')
+  .action(() => {
+    task = copyFiles();
+  })
+  .command('targets <targets>')
+  .action(targets => {
+    task = buildTargets(targets.split(','));
+  })
+  .command('rollup <config>', config => {
+    task = buildRollup(path.join(process.cwd(), config));
+  });
 
-// mocked Listr
-let task = {
-  run: () => Promise.reject(new Error(program.usage())),
-};
-
-if (program.full) {
-  task = fullBuild(program.targets, { rollupConfig: program.rollup });
-} else if (program.targets) {
-  task = buildTargets(program.targets);
-} else if (program.rollup) {
-  task = buildRollup(path.join(process.cwd(), program.rollup));
-} else if (program.copyFiles) {
-  task = copyFiles();
-}
+program.parse(process.argv);
 
 task.run().catch(err => {
   console.error(err);
