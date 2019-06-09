0a5205b8ff54577384c96f0773d1307b7c34bbfc
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-09 15:37:21 +0200

    Dry run

--- scripts/sizeSnapshot/upload.js
+++ scripts/sizeSnapshot/upload.js
@@ -30,7 +30,7 @@ async function main() {
 
   const branch = process.env.BUILD_SOURCEBRANCHNAME;
 
-  const snapshot = await fse.readJSON(snapshotDestPath);
+  const snapshot = {}; // await fse.readJSON(snapshotDestPath);
 
   function upload(revision) {
     const uploadOptions = {
