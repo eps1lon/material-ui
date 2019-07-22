0a8e953d3e4e624bacc3b9d395e379bb5e1d4147
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-22 15:32:54 +0000

    Revert "Bump raw-loader from 1.0.0 to 3.1.0 (#63)"
    This reverts commit d7b1b9ec6ef78a81bfda086a523848ab95e0e030.

--- docs/src/modules/utils/mapTranslations.js
+++ docs/src/modules/utils/mapTranslations.js
@@ -1,21 +1,12 @@
 export default function mapTranslations(req, ext) {
   const translations = {};
-  function load(filename) {
-    // .md is required by raw-loader which uses ES6 modules
-    // .json is required with default require semantics which use commonJS
-    const esModuleRequire = ext === 'md';
-    if (esModuleRequire) {
-      return req(filename).default;
-    }
-    return req(filename);
-  }
   req.keys().forEach(filename => {
     const match = filename.match(new RegExp(`-([a-z]{2}).${ext}$`));
 
     if (match) {
-      translations[match[1]] = load(filename);
+      translations[match[1]] = req(filename);
     } else {
-      translations.en = load(filename);
+      translations.en = req(filename);
     }
   });
   return translations;
