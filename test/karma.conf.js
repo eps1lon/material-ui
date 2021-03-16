b1b99948b18a84398186b9cf8e0d2475c024726f
eps1lon <silbermann.sebastian@gmail.com>
2021-03-16 11:48:51 +0100

    Same conf as repro

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,21 +1,4 @@
 const playwright = require('playwright');
-const webpack = require('webpack');
-
-const CI = Boolean(process.env.CI);
-
-let build = `material-ui local ${new Date().toISOString()}`;
-
-if (process.env.CIRCLE_BUILD_URL) {
-  build = process.env.CIRCLE_BUILD_URL;
-}
-
-const browserStack = {
-  username: process.env.BROWSERSTACK_USERNAME,
-  accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
-  build,
-  // https://github.com/browserstack/api#timeout300
-  timeout: 5.5 * 60, // Maximum time before a worker is terminated. Default 5 minutes.
-};
 
 process.env.CHROME_BIN = playwright.chromium.executablePath();
 
@@ -25,54 +8,23 @@ module.exports = function setKarmaConfig(karmaConfig) {
     browsers: ['chromeHeadless'],
     frameworks: ['mocha', 'webpack'],
     files: [{ pattern: 'test/karma.tests.js', watched: false }],
-    plugins: ['karma-mocha', 'karma-chrome-launcher', 'karma-sourcemap-loader', 'karma-webpack'],
-    /**
-     * possible values:
-     * - config.LOG_DISABLE
-     * - config.LOG_ERROR
-     * - config.LOG_WARN
-     * - config.LOG_INFO
-     * - config.LOG_DEBUG
-     */
-    logLevel: karmaConfig.LOG_INFO,
-    port: 9876,
+    plugins: ['karma-mocha', 'karma-chrome-launcher', 'karma-webpack'],
     preprocessors: {
-      'test/karma.tests.js': ['webpack', 'sourcemap'],
+      'test/karma.tests.js': ['webpack'],
     },
-    proxies: {
-      '/fake.png': '/base/test/assets/fake.png',
-      '/fake2.png': '/base/test/assets/fake2.png',
-    },
-    reporters: ['dots'],
     webpack: {
-      devtool: CI ? 'inline-source-map' : 'eval-source-map',
-      module: {
-        rules: [
-          {
-            test: /\.(js|ts|tsx)$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            options: {
-              envName: 'test',
-            },
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
-        fallback: {
-          // Some tests import fs
-          fs: false,
-          // required by source-map-support
-          path: require.resolve('path-browserify'),
-          // required by e.g. styled-components
-          stream: false,
-        },
-      },
-    },
-    webpackMiddleware: {
-      noInfo: true,
-      writeToDisk: CI,
+      // module: {
+      //   rules: [
+      //     {
+      //       test: /\.(js|ts|tsx)$/,
+      //       loader: 'babel-loader',
+      //       exclude: /node_modules/,
+      //       options: {
+      //         envName: 'test',
+      //       },
+      //     },
+      //   ],
+      // },
     },
     customLaunchers: {
       chromeHeadless: {
